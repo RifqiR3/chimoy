c99de6255d500a98608d2c38f21bbf8e52ea41b2
Rifqi <rifkyirwan68@gmail.com>
2023-10-24 20:38:28 +0800

    tambah page menu, kasir_page, sama konfirmasi pembayaran kasir (kasir_page2)

---
+++ app/src/main/java/com/example/mobileumkm/kasir_page2.java
@@ -0,0 +1,18 @@
+package com.example.mobileumkm;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;  // Import TextView
+import android.graphics.Paint;  // Import Paint
+
+import android.os.Bundle;
+
+public class kasir_page2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kasir_page2);
+
+
+    }
+}
