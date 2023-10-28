08ca807bd7c39dce40dd5071a7e74de2705bb693
Rifqi <rifkyirwan68@gmail.com>
2023-10-28 20:24:22 +0800

    Perbaiki halaman laporan keuangan

--- app/src/main/java/com/example/mobileumkm/MoneyFragment.java
+++ app/src/main/java/com/example/mobileumkm/MoneyFragment.java
@@ -4,13 +4,49 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 public class MoneyFragment extends Fragment {
+    private CardView cardViewHariIni;
+    private CardView cardViewMingguIni;
+    private CardView cardViewBulanIni;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_laporan_keuangan_page, container, false);
+        View view = inflater.inflate(R.layout.activity_laporan_keuangan_page, container, false);
+
+        cardViewHariIni = view.findViewById(R.id.cardViewHariIni);
+        cardViewMingguIni = view.findViewById(R.id.cardViewMingguIni);
+        cardViewBulanIni = view.findViewById(R.id.cardViewBulanIni);
+
+        cardViewHariIni.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cardViewHariIni.setCardBackgroundColor(getResources().getColor(R.color.kuningJadi));
+                cardViewMingguIni.setCardBackgroundColor(getResources().getColor(R.color.kuningPucat));
+                cardViewBulanIni.setCardBackgroundColor(getResources().getColor(R.color.kuningPucat));
+            }
+        });
+
+        cardViewMingguIni.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cardViewHariIni.setCardBackgroundColor(getResources().getColor(R.color.kuningPucat));
+                cardViewMingguIni.setCardBackgroundColor(getResources().getColor(R.color.kuningJadi));
+                cardViewBulanIni.setCardBackgroundColor(getResources().getColor(R.color.kuningPucat));
+            }
+        });
+
+        cardViewBulanIni.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cardViewHariIni.setCardBackgroundColor(getResources().getColor(R.color.kuningPucat));
+                cardViewMingguIni.setCardBackgroundColor(getResources().getColor(R.color.kuningPucat));
+                cardViewBulanIni.setCardBackgroundColor(getResources().getColor(R.color.kuningJadi));
+            }
+        });
+
+        return view;
     }
 }
--- app/src/main/java/com/example/mobileumkm/laporan_keuangan_page.java
+++ app/src/main/java/com/example/mobileumkm/laporan_keuangan_page.java
@@ -14,6 +14,7 @@ public class laporan_keuangan_page extends AppCompatActivity {
     private CardView cardViewMingguIni;
     private CardView cardViewBulanIni;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
