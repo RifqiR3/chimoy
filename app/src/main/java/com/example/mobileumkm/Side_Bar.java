c809596a10e9ad9b945c4811dfe0fdcba06c4727
Rifqi <rifkyirwan68@gmail.com>
2023-10-28 20:06:34 +0800

    Sidebar Dinamis

--- app/src/main/java/com/example/mobileumkm/Side_Bar.java
+++ app/src/main/java/com/example/mobileumkm/Side_Bar.java
@@ -12,12 +12,16 @@ import android.widget.Toast;
 import com.google.android.material.navigation.NavigationView;
 public class Side_Bar extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawerLayout;
+    private Toolbar toolbar; // Tambah referensi toolbar
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_side_bar);
-        Toolbar toolbar = findViewById(R.id.toolbar); //Ignore red line errors
+
+        Toolbar toolbar = findViewById(R.id.toolbar); //Inisialisasi toolbar
         setSupportActionBar(toolbar);
+
         drawerLayout = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
@@ -28,33 +32,47 @@ public class Side_Bar extends AppCompatActivity implements NavigationView.OnNavi
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new kasir_frag()).commit();
             navigationView.setCheckedItem(R.id.nav_home);
+            setToolbarTitle("Kasir"); // set judul toolbar awal
         }
     }
+
+    // set judul toolbar awal
+    private void setToolbarTitle(String title) {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(title);
+        }
+    }
+
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
         if  (id == R.id.nav_home){
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new kasir_frag()).commit();
+            setToolbarTitle("Kasir");
             return  true;
         }
 
         if  (id == R.id.nav_shop){
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new kasir_frag()).commit();
+            setToolbarTitle("Kasir");
             return  true;
         }
 
         if  (id == R.id.nav_history){
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HistoryFragment()).commit();
+            setToolbarTitle("Riwayat");
             return  true;
         }
 
         if  (id == R.id.nav_money){
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MoneyFragment()).commit();
+            setToolbarTitle("Laporan Keuangan");
             return  true;
         }
 
         if  (id == R.id.nav_box){
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProductFragment()).commit();
+            setToolbarTitle("Kasir");
             return  true;
         }
 
