40b1d07d38d09f61ec7a37ae327b6546f82d1424
Rifqi <rifkyirwan68@gmail.com>
2023-10-29 17:18:25 +0800

    Tambah splash screen

--- app/src/main/java/com/example/mobileumkm/landing_page.java
+++ app/src/main/java/com/example/mobileumkm/landing_page.java
@@ -3,6 +3,9 @@ package com.example.mobileumkm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.content.Intent;
+
 
 public class landing_page extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class landing_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(landing_page.this, login_page.class);
+                startActivity(intent);
+            }
+        }, 2000);
     }
 }
