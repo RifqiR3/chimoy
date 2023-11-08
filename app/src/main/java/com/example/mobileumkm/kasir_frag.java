5eea3c43d9c52e07df8ad91c08395c73b46ff23a
Rifqi <rifkyirwan68@gmail.com>
2023-11-08 20:06:50 +0800

    update tampilan kasir

--- app/src/main/java/com/example/mobileumkm/kasir_frag.java
+++ app/src/main/java/com/example/mobileumkm/kasir_frag.java
@@ -4,14 +4,66 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.view.Gravity;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 public class kasir_frag extends Fragment {
+    private TextView txtMenu1;
+    private TextView txtHrgMenu1;
+    private TextView txtCartMenu1;
+    private TextView txtCartItem1;
+
+    private TextView txtCartHarga1;
+    private LinearLayout buttonContainer;
+    private Button btnMenu1;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_kasir_page, container, false);
+        View view = inflater.inflate(R.layout.activity_kasir_page, container, false);
+
+        txtMenu1 = view.findViewById(R.id.namaMakanan1);
+        txtHrgMenu1 = view.findViewById(R.id.hargaMakanan1);
+        txtCartMenu1 = view.findViewById(R.id.menuKeranjang1);
+        txtCartItem1 = view.findViewById(R.id.jmlItem1);
+        txtCartHarga1 = view.findViewById(R.id.hrgItem1);
+
+        buttonContainer = view.findViewById(R.id.buttonContainer);
+        btnMenu1 = view.findViewById(R.id.buttonMakanan1);
+        btnMenu1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Buat button
+//                Button bayarButton = new Button(getContext());
+//                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+//                        LinearLayout.LayoutParams.WRAP_CONTENT,
+//                        LinearLayout.LayoutParams.WRAP_CONTENT
+//                );
+//
+//                layoutParams.gravity = Gravity.CENTER;
+//                bayarButton.setLayoutParams(layoutParams);
+//
+//                bayarButton.setText("Keranjang");
+//                bayarButton.setTextColor(getResources().getColor(R.color.black));
+//                bayarButton.setBackgroundColor(getResources().getColor(R.color.kuningJadi));
+//
+//                buttonContainer.addView(bayarButton);
+                txtCartMenu1.setText(txtMenu1.getText());
+                txtCartHarga1.setText(txtHrgMenu1.getText());
+                txtCartItem1.setText("1 Item");
+
+                buttonContainer.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+        return view;
     }
 }
