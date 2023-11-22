40ccc592bac7a31aabce395bd06c23623b3db866
wahyu <wahyuhidayah221203@gmail.com>
2023-11-22 21:40:32 +0800

    baru sudah buat konek si database firebash

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.example.mobileumkm"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.mobileumkm"
@@ -31,11 +32,14 @@ android {
     }
 }
 
+
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-database:20.3.0")
+    implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/mobileumkm/Side_Bar.java
+++ app/src/main/java/com/example/mobileumkm/Side_Bar.java
@@ -8,11 +8,13 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 import com.google.android.material.navigation.NavigationView;
 public class Side_Bar extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawerLayout;
     private Toolbar toolbar; // Tambah referensi toolbar
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,35 +52,41 @@ public class Side_Bar extends AppCompatActivity implements NavigationView.OnNavi
         if  (id == R.id.nav_home){
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new kasir_frag()).commit();
             setToolbarTitle("Kasir");
+            drawerLayout.closeDrawer(GravityCompat.START);
             return  true;
         }
 
         if  (id == R.id.nav_shop){
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new kasir_page2()).commit();
             setToolbarTitle("Keranjang");
+            drawerLayout.closeDrawer(GravityCompat.START);
             return  true;
         }
 
         if  (id == R.id.nav_history){
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HistoryFragment()).commit();
             setToolbarTitle("Riwayat");
+            drawerLayout.closeDrawer(GravityCompat.START);
             return  true;
         }
 
         if  (id == R.id.nav_money){
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MoneyFragment()).commit();
             setToolbarTitle("Laporan Keuangan");
+            drawerLayout.closeDrawer(GravityCompat.START);
             return  true;
         }
 
         if  (id == R.id.nav_box){
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new top_produk()).commit();
             setToolbarTitle("Top Produk");
+            drawerLayout.closeDrawer(GravityCompat.START);
             return  true;
         }
 
         if  (id == R.id.nav_logout){
             Toast.makeText(this, "Logout!", Toast.LENGTH_SHORT).show();
+            drawerLayout.closeDrawer(GravityCompat.START);
             return  true;
         }
 
