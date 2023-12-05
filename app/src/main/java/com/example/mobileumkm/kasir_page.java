558aadb43868b4371b8adea48beb4a89d5934bdb
wahyu <wahyuhidayah221203@gmail.com>
2023-12-05 18:42:12 +0800

    masih proses

--- app/src/main/java/com/example/mobileumkm/kasir_page.java
+++ app/src/main/java/com/example/mobileumkm/kasir_page.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.recyclerview.widget.RecyclerView;
 import com.example.mobileumkm.Adapter.AdapterMenu;
@@ -16,21 +17,27 @@ import java.util.ArrayList;
 
 
 public class kasir_page extends AppCompatActivity {
-    FloatingActionButton tambah;
+    FloatingActionButton floatingActionButton;
     AdapterMenu adapterMenu;
     ArrayList<GetDataProduk> model;
-    RecyclerView tv_listmenu;
+//    RecyclerView tv_listmenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kasir_page);
 
-        tambah = findViewById(R.id.btn_tambahdata);
-        tv_listmenu = findViewById(R.id.tv_listmenu);
-        load_data();
-        AdapterMenu adapterMenu = new AdapterMenu(getApplicationContext(), model);
-        tv_listmenu.setAdapter(adapterMenu);
+        floatingActionButton = findViewById(R.id.btn_tambahdata);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(kasir_page.this, TambahMenu.class));
+            }
+        });
+//        tv_listmenu = findViewById(R.id.tv_listmenu);
+//        load_data();
+//        AdapterMenu adapterMenu = new AdapterMenu(getApplicationContext(), model);
+//        tv_listmenu.setAdapter(adapterMenu);
 
     }
     void load_data(){
